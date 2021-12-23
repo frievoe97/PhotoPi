b8ee1877b10dbb0b52ad9fec66d5f89405a29714
Friedrich Völkers <friedrich.voelkers@me.com>
2021-12-23 14:59:13 +0100

    New Main.py

---
+++ main2.py
@@ -0,0 +1,27 @@
+# Import required Libraries
+from tkinter import *
+from PIL import Image, ImageTk
+import cv2
+
+# Create an instance of TKinter Window or frame
+win = Tk()
+
+# Set the size of the window
+win.geometry("700x350")
+
+# Create a Label to capture the Video frames
+label =Label(win)
+label.grid(row=0, column=0)
+cap= cv2.VideoCapture(0)
+
+# Define function to show frame
+def show_frames():
+   cv2image= cv2.cvtColor(cap.read()[1],cv2.COLOR_BGR2RGB)
+   img = Image.fromarray(cv2image)
+   imgtk = ImageTk.PhotoImage(image = img)
+   label.imgtk = imgtk
+   label.configure(image=imgtk)
+   label.after(20, show_frames)
+
+show_frames()
+win.mainloop()
